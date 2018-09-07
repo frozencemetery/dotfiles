2fdb14a44aee285f5cedf2f9d6e4c7d36a38e6d1
Robbie Harwood <rharwood@redhat.com>
2018-09-07 15:09:57 -0400

    xmonad config

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,194 @@
+import System.IO
+
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Layout.NoBorders
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
+import XMonad.Layout.MultiColumns
+import XMonad.Layout.PerWorkspace
+import XMonad.Hooks.UrgencyHook
+import XMonad.Util.EZConfig
+
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
+import qualified XMonad.Util.Loggers as L
+
+spawnterm :: String -> X ()
+spawnterm c = spawn $ "urxvtcd -e " ++ c
+
+mpc :: String -> X ()
+mpc c = spawn $ "MPD_HOST=/run/mpd/socket mpc " ++ c
+
+dmenu :: String -> String -> X ()
+dmenu command prompt = let
+  cfg = " -b -fn Terminus:size=8 -nb black -nf grey -sb orange -sf black -p "
+  in spawn $ command ++ cfg ++ prompt
+
+ydl :: X ()
+ydl = let
+  fmtstr = "'bestvideo[height<=1080][width<=1920]+bestaudio/best'"
+  ytdl = "youtube-dl -f " ++ fmtstr ++ " -o - "
+  sink = " | mplayer - >/dev/null"
+  in spawn $ ytdl ++ "$(xsel -o)" ++ sink
+
+widthdelta = 3/100 :: Rational
+special = multiCol [1, 1] 0 widthdelta (1/3) :: MultiCol a
+regular = multiCol [1, 1, 1] 0 widthdelta (1/4) :: MultiCol a
+myLayouts = avoidStruts $ smartBorders $
+            mkToggle (single FULL) . mkToggle (single MIRROR) $
+            onWorkspace "1" (special ||| regular) $
+            (regular ||| special)
+
+keymap :: XConfig Layout -> M.Map (KeyMask, KeySym) (X())
+keymap conf@XConfig {XMonad.modMask = modm} = let
+  ordered_keysyms = [xK_1 .. xK_9] ++ [xK_0]
+  wspacekeys =
+    [ ((m .|. modm, k), windows $ f i)
+    | (i, k) <- zip (XMonad.workspaces conf) ordered_keysyms
+    , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+    ] ++
+    [ ( (m .|. modm .|. mod1Mask, key)
+      , screenWorkspace sc >>= flip whenJust (windows . f)
+      )
+    | (key, sc) <- zip ordered_keysyms [0..]
+    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+    ]
+  keyconf =
+    [ ("M-S-c", kill)
+    , ("M-q", spawn "pkill .mpdmonitor.sh; xmonad --restart")
+    , ("M-b", sendMessage ToggleStruts)
+    , ("M-r", refresh)
+
+    -- windows
+    , ("M-n", windows W.focusDown)
+    , ("M-S-n", windows W.swapDown)
+    , ("M-p", windows W.focusUp)
+    , ("M-S-p", windows W.swapUp)
+    , ("M-h", sendMessage Shrink)
+    , ("M-l", sendMessage Expand)
+    , ("M-m", windows W.focusMaster)
+
+    -- layouts
+    , ("M-,", sendMessage $ IncMasterN 1)
+    , ("M-.", sendMessage $ IncMasterN $ -1)
+    , ("M-<Space>", sendMessage NextLayout)
+    , ("M-S-<Space>", setLayout $ XMonad.layoutHook conf)
+    , ("M-f", sendMessage $ Toggle FULL)
+    , ("M-a", sendMessage $ Toggle MIRROR)
+    , ("M-t", withFocused $ windows . W.sink)
+
+    -- spawn all the things
+    , ("M-S-<Return>", spawn $ terminal conf)
+    , ("M-<Return>", spawnterm "emacsclient -nw -a emacs")
+    , ("M-S-x", spawn "gmrun")
+    , ("M-x", dmenu "dmenu_run" "cmd: ")
+    , ("M-u", spawn "dmenu-haskey")
+    , ("M-y", ydl)
+    , ("M-S-/", spawnterm "less /usr/share/X11/locale/en_US.UTF-8/Compose")
+    , ("M-w", spawnterm "emacsclient -nw $(mktemp)")
+    , ("M-S-a", spawnterm "htop")
+
+    -- "regular" bindings
+    , ("<XF86AudioPrev>", mpc "pause")
+    , ("<XF86AudioPlay>", spawnterm "alsamixer")
+    , ("<XF86AudioNext>", mpc "play")
+    , ("S-<XF86AudioPrev>", mpc "prev")
+    , ("S-<XF86AudioPlay>", spawnterm "ncmpcpp")
+    , ("S-<XF86AudioNext>", mpc "clear")
+    , ("<XF86AudioLowerVolume>", spawn "amixer set Master 3%-")
+    , ("<XF86AudioRaiseVolume>", spawn "amixer set Master 3%+")
+    , ("<XF86WebCam>", spawn "mplayer -vf mirror tv://")
+
+    -- T540p bindings
+    , ("<XF86Search>", mpc "pause")
+    , ("<XF86LaunchA>", spawnterm "alsamixer")
+    , ("<XF86Explorer>", mpc "play")
+    , ("S-<XF86Search>", mpc "prev")
+    , ("S-<XF86LaunchA>", spawnterm "ncmpcpp")
+    , ("S-<XF86Explorer>", mpc "clear")
+
+    -- Amazon keyboard bindings
+    , ("<XF86AudioStop>", mpc "pause")
+    , ("S-<XF86AudioStop>", mpc "prev")
+    , ("<XF86Calculator>", spawn "xscreensaver-command --lock")
+
+    -- Apple Extended II bindings
+    , ("C-<F2>", spawn "amixer set Master 3%-")
+    , ("C-<F3>", spawn "amixer set Master 3%+")
+    , ("C-<F10>", mpc "pause")
+    , ("C-<F11>", spawnterm "alsamixer")
+    , ("C-<F12>", mpc "play")
+    , ("C-S-<F10>", mpc "prev")
+    , ("C-S-<F11>", spawnterm "ncmpcpp")
+    , ("C-S-<F12>", mpc "clear")
+    , ("<Pause>", spawn "xscreensaver-command --lock")
+    ]
+  in (mkKeymap conf keyconf) `M.union` (M.fromList wspacekeys)
+
+mice :: XConfig l0 -> M.Map (KeyMask, Button) (Window -> X())
+mice XConfig {XMonad.modMask = modm} = let
+  move w = focus w >> mouseMoveWindow w >> windows W.shiftMaster
+  resize w = focus w >> mouseResizeWindow w >> windows W.shiftMaster
+  master w = focus w >> windows W.shiftMaster
+  actions = [ ((modm, button1), move)
+            , ((modm, button2), resize)
+            , ((modm, button3), master)
+            ]
+  in M.fromList actions
+
+logger :: Handle -> X ()
+logger xmproc = let
+  workspacePP "MultiCol" = "m │"
+  workspacePP "Full" = "ƒ │"
+  workspacePP "ResizableTall" = "t │"
+  workspacePP "Mirror MultiCol" = "M │"
+  workspacePP "Mirror ResizableTall" = "T │"
+  workspacePP s = s
+
+  xmpp = xmobarPP
+    { ppCurrent = xmobarColor "black" "orange" . xmobarStrip
+    , ppVisible = xmobarColor "black" "white" . xmobarStrip
+    , ppWsSep = ""
+    , ppHidden = xmobarColor "black" "grey" . xmobarStrip
+    , ppHiddenNoWindows = id
+    , ppUrgent = xmobarColor "orange" "black" . xmobarStrip
+    , ppSep = " "
+    , ppTitle = xmobarColor "purple" "black" . xmobarStrip
+    , ppLayout = workspacePP
+    , ppOutput = hPutStrLn xmproc
+    }
+  in dynamicLogWithPP xmpp
+
+manip :: ManageHook
+manip = let
+  action = [ title =? "MPlayer" --> doFloat
+           , resource =? "Dialog" --> doFloat
+           , className =? "Qjackctl" --> doFloat
+           , className =? "Qjackctl.real" --> doFloat
+           , className =? "MPlayer" --> doFloat
+           , className =? "pinentry" --> doFloat
+           ]
+  in manageDocks <+> composeAll action
+
+main :: IO ()
+main = do
+  xmproc <- spawnPipe "xmobar"
+  xmonad $ withUrgencyHook NoUrgencyHook def
+    { terminal = "urxvtcd"
+    , focusFollowsMouse = False
+    , borderWidth = 1
+    , modMask = mod4Mask
+    , workspaces = map show $ [1..9] ++ [0]
+    , normalBorderColor = "#000000"
+    , focusedBorderColor = "#ff7228"
+    , keys = keymap
+    , mouseBindings = mice
+    , layoutHook = myLayouts
+    , logHook = logger xmproc
+    , manageHook = manip
+    , handleEventHook = docksEventHook
+    , startupHook = spawnterm "zsh"
+    }
