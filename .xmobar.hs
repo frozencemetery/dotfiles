e0511b9a6200de00410cb741df5015c7845cd6d6
Robbie Harwood <rharwood@redhat.com>
2020-11-17 11:10:46 -0500

    Move monitor scripts into a dedicated repo

--- .xmobar.hs
+++ .xmobar.hs
@@ -1,3 +1,5 @@
+-- monitor scripts: https://github.com/frozencemetery/monitors
+
 Config { font = "xft:Terminus:size=9"
        , bgColor = "black"
        , fgColor = "grey"
@@ -6,10 +8,10 @@ Config { font = "xft:Terminus:size=9"
        , commands = [ Run MultiCpu ["-t", "(<total0>, <total1>, <total2>, <total3>, <total4>, <total5>, <total6>, <total7>, <total8>, <total9>, <total10>, <total11>)", "-L", "25", "-H", "75", "--normal", "white", "--high", "orange", "-m", "3", "-w", "3", "-S", "true"] 10
                     , Run Memory ["-t", "<used>MB", "-L", "6000", "-H", "15000", "--normal", "white", "--high", "orange", "-c", "0", "-m", "5", "-w", "5"] 10
                     , Run Date "%a %Y %b %d %R" "date" 10
-                    , Run CommandReader "exec /home/bos/rharwood/.batterymonitor.py" "battery"
+                    , Run CommandReader "exec batterymonitor.py" "battery"
                     , Run StdinReader
-                    , Run CommandReader "exec /home/bos/rharwood/.mpdmonitor.sh xmobar" "MPD"
-                    , Run CommandReader "exec /home/bos/rharwood/.alsamonitor.py" "alsa"
+                    , Run CommandReader "exec mpdmonitor.sh xmobar" "MPD"
+                    , Run CommandReader "exec alsamonitor.py" "alsa"
                     ]
        , sepChar = "%"
        , alignSep = "}{"
